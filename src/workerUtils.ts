1e05f3b0bf9d6c65d1bcba18cb04d2cfb0cdd059
Amir Yuval <amiryu@qwilt.com>
2021-10-21 00:39:43 +0300

    browser support check

--- src/workerUtils.ts
+++ src/workerUtils.ts
@@ -1,5 +1,9 @@
 export function createWorker(worker: Function) {
-  const code = worker.toString();
-  const blob = new Blob(["(" + code + ")()"]);
-  return new Worker(URL.createObjectURL(blob));
+  if(Worker) {
+    const code = worker.toString();
+    const blob = new Blob(["(" + code + ")()"]);
+    return new Worker(URL.createObjectURL(blob));
+  } else {
+    console.error('Your browser doesn\'t support web workers.');
+  }
 }
